56e25a4ff9fc30da65f00e33fe24673ca9d6b29d
Julian Narvaez <oscarnarvaez01@hotmail.com>
2020-06-02 17:05:30 -0500

    add cors

--- index.js
+++ index.js
@@ -25,6 +25,20 @@ let heroController = require('./app/utils/controller')('heroes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
+
+var whitelist = ['http://localhost','https://devlearning-2c9c9.web.app','*'];
+
+var corsOptions = {
+    origin: function(origin, callback){
+        if(whitelist.indexOf(origin) !== -1){
+            callback(null, true)
+        }else{
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+};
+
 app.use(cors());
 
 app.use('/heroes',heroController);
